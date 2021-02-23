d579eb03f39db8b55a28635b8c5b0f8de6f3b5e8
elliefh <elliefu18@gmail.com>
2021-02-23 12:19:04 -0800

    set up port, express app, app listener

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 // Dependencies
 const express = require("express");
 
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
+
+// Create express app instance.
+const app = express();
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
