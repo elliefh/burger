b6f96a42422433f50ad6f4c56315e2bef4190f1b
elliefh <elliefu18@gmail.com>
2021-02-23 12:19:56 -0800

    created table for burgers

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INTEGER(11) AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(30),
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN,
     PRIMARY KEY (id)
 );
