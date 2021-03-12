0c9061adfd6175ada924fc4f396d673434a00cd1
elliefh <elliefu18@gmail.com>
2021-03-12 13:11:18 -0800

    updated connection to launch on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 // Dependencies
 const mysql = require("mysql");
 require("dotenv").config();
+let connection;
 
-// Create MySQL connection
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: process.env.PORT || 3306,
   user: "root",
   password: process.env.DB_PASSWORD,
   database: "burgers_db"
-});
+  });
+}
 
 connection.connect(function(err) {
   if (err) throw err;
