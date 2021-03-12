174c2733d562f0c1a6117883722fe7263026ac0f
elliefh <elliefu18@gmail.com>
2021-03-12 13:04:34 -0800

    added default value to devoured boolean

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INTEGER(11) AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(30),
-    devoured BOOLEAN,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
